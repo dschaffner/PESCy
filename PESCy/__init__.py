829ec10769aa009eabef405b00db53e6eee494dc
dschaffner <dschaffner@brynmawr.edu>
2021-06-18 12:48:11 -0400

    Create __init__.py

---
+++ PESCy/__init__.py
@@ -0,0 +1,13 @@
+#
+# This file is part of the PESCy technique package
+#
+#
+#
+#
+
+"""
+The PESCy package
+"""
+__all__ = ["Cmaxmin","calcCofH","calcH","calcH_fromPatternCount","calcPESCcurves","calcS","calcS_fromPatternCount","constructPatternCount"]
+
+from PESCy import Cmaxmin,calcCofH,calcH,calcH_fromPatternCount,calcPESCcurves,calcS,calcS_fromPatternCount,constructPatternCount
