407ddb58ba1d3f60ccab299bfa060fc10dbf0c67
dschaffner <dschaffner@brynmawr.edu>
2021-06-18 12:41:43 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,54 @@
+# This file is part of the PESCy technique package
+#
+
+__all__ = ["PESCy"]
+
+import pkg_resources
+
+from bmplib import (PESCy)
+
+# --- Define version -----------------------------------------------------------
+try:
+    # this places a runtime dependency on setuptools
+    #
+    # note: if there's any distribution metadata in your source files, then this
+    #       will find a version based on those files.  Keep distribution metadata
+    #       out of your repository unless you've intentionally installed the package
+    #       as editable (e.g. `pip install -e {PESCy_directory_root}`),
+    #       but then __version__ will not be updated with each commit, it is
+    #       frozen to the version at time of install.
+    #
+    #: `PESCy` version string
+    __version__ = pkg_resources.get_distribution("PESCy").version
+except pkg_resources.DistributionNotFound:
+    # package is not installed
+    fallback_version = "unknown"
+    try:
+        # code most likely being used from source
+        # if setuptools_scm is installed then generate a version
+        from setuptools_scm import get_version
+
+        __version__ = get_version(
+            root="..", relative_to=__file__, fallback_version=fallback_version
+        )
+        del get_version
+        warn_add = "setuptools_scm failed to detect the version"
+    except ModuleNotFoundError:
+        # setuptools_scm is not installed
+        __version__ = fallback_version
+        warn_add = "setuptools_scm is not installed"
+
+    if __version__ == fallback_version:
+        from warnings import warn
+
+        warn(
+            f"PESCy.__version__ not generated (set to 'unknown'), PESCy is "
+            f"not an installed package and {warn_add}.",
+            RuntimeWarning,
+        )
+
+        del warn
+    del fallback_version, warn_add
+
+
+del pkg_resources
