dab9d7491b243227a34a23dd9a272af6397ed23c
dschaffner <dschaffner@brynmawr.edu>
2021-06-18 13:58:53 -0400

    Update __init__.py

--- PESCy/__init__.py
+++ PESCy/__init__.py
@@ -10,4 +10,4 @@ The PESCy package
 """
 __all__ = ["Cmaxmin","calcCofH","calcH","constructPatternCount","calcS_fromPatternCount","calcPESCcurves","calcS"]
 
-from PESCy import Cmaxmin,calcCofH,calcH,calcS,constructPatternCount,calcS_fromPatternCount,calcPESCcurves,
+from PESCy import Cmaxmin,calcCofH,calcH,calcS,constructPatternCount,calcS_fromPatternCount,calcPESCcurves
