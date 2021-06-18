6c2a2d2979a16e5b2ac21c69380286fd658bb6a7
dschaffner <dschaffner@brynmawr.edu>
2021-06-18 12:36:44 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+# This file is part of the PESCy technique package
+#
+#
+#
+#
+import os
+
+from setuptools import setup
+
+# find here
+here = os.path.abspath(os.path.dirname(__file__))
+
+# ---- Perform setup                                                        ----
+setup(use_scm_version=True)
