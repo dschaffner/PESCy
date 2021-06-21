29dc613b12759fad7ac4e68809550e0d9bacaa98
David Schaffner <dschaffner@brynmawr.edu>
2021-06-21 00:59:13 -0400

    Create timeseries.py

---
+++ PESCy/timeseries.py
@@ -0,0 +1,56 @@
+
+"""Timeseries functions for testing PESCy"""
+__all__ = [
+    "lorenz",
+    "lorenz_mod",
+    "generateLorenz"
+]
+
+import numpy as np
+
+def lorenz(x, y, z, s=10, r=20, b=2.667):
+    """
+    Given:
+       x, y, z: a point of interest in three dimensional space
+       s, r, b: parameters defining the lorenz attractor
+    Returns:
+       x_dot, y_dot, z_dot: values of the lorenz attractor's partial
+           derivatives at the point x, y, z
+    """
+    x_dot = s*(y - x)
+    y_dot = r*x - y - x*z
+    z_dot = x*y - b*z
+    return x_dot, y_dot, z_dot
+
+def lorenz_mod(x, y, z, s=10, r=28, b=2.667):
+    """
+    Given:
+       x, y, z: a point of interest in three dimensional space
+       s, r, b: parameters defining the lorenz attractor
+    Returns:
+       x_dot, y_dot, z_dot: values of the lorenz attractor's partial
+           derivatives at the point x, y, z
+    """
+    x_dot = s*(y - x)
+    y_dot = 2.3*r*x - y - x*z
+    z_dot = x*y - b*z
+    return x_dot, y_dot, z_dot
+
+def generateLorenz(dt=0.01,num_steps=10000,s=10,r=20,b=2.667):
+    # Need one more for the initial values
+    xs = np.empty(num_steps + 1)
+    ys = np.empty(num_steps + 1)
+    zs = np.empty(num_steps + 1)
+    
+    # Set initial values
+    xs[0], ys[0], zs[0] = (0., 1., 1.05)
+    
+    # Step through "time", calculating the partial derivatives at the current point
+    # and using them to estimate the next point
+    for i in range(num_steps):
+        x_dot, y_dot, z_dot = lorenz(xs[i], ys[i], zs[i])
+        xs[i + 1] = xs[i] + (x_dot * dt)
+        ys[i + 1] = ys[i] + (y_dot * dt)
+        zs[i + 1] = zs[i] + (z_dot * dt)
+        
+    return xs,ys,zs
