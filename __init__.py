22c9f4c77ebe13ce578e14a19d0155210de004a4
dschaffner <dschaffner@brynmawr.edu>
2021-07-19 15:23:30 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ __all__ = ["PESCy"]
 
 import pkg_resources
 
-from PESCy import PESCy
+#from PESCy import PESCy
 
 # --- Define version -----------------------------------------------------------
 try:
